514f97b529dfe90d104ca1cd27c3498d511813a2
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-27 23:59:37 -0300

    293. Criando Activity de Depositar

--- app/src/main/java/com/exemplo/bancodigital/app/MainActivity.java
+++ app/src/main/java/com/exemplo/bancodigital/app/MainActivity.java
@@ -2,9 +2,11 @@ package com.exemplo.bancodigital.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.exemplo.bancodigital.R;
+import com.exemplo.bancodigital.deposito.DepositoFormActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configCliques();
     }
+
+    private void configCliques() {
+        findViewById(R.id.cardDeposito).setOnClickListener(v -> {
+            startActivity(new Intent(this, DepositoFormActivity.class));
+        });
+    }
+
 }
---
+++ app/src/main/java/com/exemplo/bancodigital/deposito/DepositoFormActivity.java
@@ -0,0 +1,29 @@
+package com.exemplo.bancodigital.deposito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.exemplo.bancodigital.R;
+
+public class DepositoFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposito_form);
+
+        configToolbar();
+    }
+
+    private void configToolbar() {
+        TextView textTitulo = findViewById(R.id.textTitulo);
+        textTitulo.setText("Depositar");
+
+        findViewById(R.id.ibVoltar).setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+}
