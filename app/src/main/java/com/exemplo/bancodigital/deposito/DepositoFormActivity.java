802494ecfd093614ae473051f8b8b46abefd3015
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-28 01:02:45 -0300

    294. Configurando Mascara e Validando Deposito

--- app/src/main/java/com/exemplo/bancodigital/deposito/DepositoFormActivity.java
+++ app/src/main/java/com/exemplo/bancodigital/deposito/DepositoFormActivity.java
@@ -1,13 +1,28 @@
 package com.exemplo.bancodigital.deposito;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.exemplo.bancodigital.R;
 
+import java.util.Locale;
+
 public class DepositoFormActivity extends AppCompatActivity {
+
+    private CurrencyEditText edtValor;
+
+    private AlertDialog dialog;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,44 @@ public class DepositoFormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_deposito_form);
 
         configToolbar();
+
+        iniciaComponentes();
+    }
+
+    public void validaDeposito(View view) {
+        double valorDeposito = (double) edtValor.getRawValue() / 100;
+
+        if(valorDeposito > 0) {
+            ocultarTeclado();
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            // Salvar extrato
+        } else {
+            showDialog();
+        }
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.CustomAlertDialog);
+
+        View view = getLayoutInflater().inflate(R.layout.layout_dialog_info, null);
+
+        TextView textTitulo = view.findViewById(R.id.textTitulo);
+        textTitulo.setText("Atenção!");
+
+        TextView textMensagem = view.findViewById(R.id.textMensagem);
+        textMensagem.setText("Digite um valor maior que 0");
+
+        Button btnOk = view.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        builder.setView(view);
+
+        dialog = builder.create();
+        dialog.show();
     }
 
     private void configToolbar() {
@@ -24,6 +77,19 @@ public class DepositoFormActivity extends AppCompatActivity {
         findViewById(R.id.ibVoltar).setOnClickListener(v -> {
             finish();
         });
+    }
+
+    private void iniciaComponentes() {
+        edtValor = findViewById(R.id.edtValor);
+        edtValor.setLocale(new Locale("PT", "br"));
+
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    // Oculta o teclado do dispositivo
+    private void ocultarTeclado() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(edtValor.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
 }
